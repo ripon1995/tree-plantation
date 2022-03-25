217172939cc013d181043ad6601bc6a93569dc65
DESKTOP-PSR46S4\BS592 <ripon1995dc@gmail.com>
2022-03-25 21:01:36 +0600

    inital commit

---
+++ customer/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(blank=False, unique=True)
+    phone = models.CharField(max_length=20)
+    password = models.CharField(max_length=5)
+    username = models.CharField(max_length=20)
