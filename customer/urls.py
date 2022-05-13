e1f5e24ae02803af68259525fa63ba2d5422680e
Ripon <alamin.ripon@brainstation-23.com>
2022-05-13 11:58:21 +0600

    new env testing

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from customer import views
-
+# test
 urlpatterns = [
     path('customers/', views.GetCustomerList.as_view()),
     path('customers/<int:pk>/', views.GetCustomer.as_view()),
