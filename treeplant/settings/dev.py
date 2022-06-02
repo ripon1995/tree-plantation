b3eb4853363ec119a8e43a0f401b7f78c8a44e3f
Ripon <alamin.ripon@brainstation-23.com>
2022-06-02 15:59:24 +0600

    new env testing

--- treeplant/settings/dev.py
+++ treeplant/settings/dev.py
@@ -3,7 +3,8 @@ from .common import *
 DEBUG = True
 SECRET_KEY = 'django-insecure-1q0dc2(u&a7d#8e+nz!wuodgd1kt*48x!bo^(xki!e7%#=11(1'
 
-ALLOWED_HOSTS = ['172.16.226.179', 'desktop-psr46s4.brainstation-23', '10.0.2.2', '192.168.68.108', '127.0.0.1']
+ALLOWED_HOSTS = ['172.16.226.179', 'desktop-psr46s4.brainstation-23', '10.0.2.2:8080', '192.168.68.108', '127.0.0.1',
+                 '172.16.227.86', '172.16.227.255', '172.31.16.1', '0.0.0.0']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
