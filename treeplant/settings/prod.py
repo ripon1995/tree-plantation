ee25fb78bde19216938f5dc728c6f494f3f3338a
rootless <ripon1995dc@gmail.com>
2022-07-06 15:26:14 +0600

    What? Deployment. How? 1. added heroku heroku link in the allowed host field

--- treeplant/settings/prod.py
+++ treeplant/settings/prod.py
@@ -4,4 +4,4 @@ import os
 DEBUG = False
 SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
 # SECRET_KEY = os.environ['SECRET_KEY']
-ALLOWED_HOSTS = ['treeplantation.herokuapp.com']
+ALLOWED_HOSTS = ['treeplantation-backend.herokuapp.com']
