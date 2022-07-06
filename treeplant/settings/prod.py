f1fabf08df389fc873d394ae8045870bdb40ab99
rootless <ripon1995dc@gmail.com>
2022-07-06 16:21:23 +0600

    What? Deployment. How? 1. added postgres database provision Heroku Postgres 2. installed dj-database-url 3. updated requirements.txt file

--- treeplant/settings/prod.py
+++ treeplant/settings/prod.py
@@ -1,7 +1,12 @@
 from .common import *
 import os
+import dj_database_url
 
 DEBUG = False
 SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
 # SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['treeplantation-backend.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
