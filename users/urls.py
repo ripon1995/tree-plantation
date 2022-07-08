773782f2d8c3e722f9aed1756756babfe25d668d
rootless <ripon1995dc@gmail.com>
2022-07-08 17:59:35 +0600

    What? api implementation : users/me/ How? 1. added path 'users/me/'

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CustomRegistrationView
+from .views import CustomRegistrationView, CustomProfileView
 
 urlpatterns = [
     path('users/', CustomRegistrationView.as_view()),
+    path('users/me/', CustomProfileView.as_view())
 ]
