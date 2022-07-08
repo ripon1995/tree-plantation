213ebd70e26d4b453173f4f3d40d907e2aa339d1
rootless <ripon1995dc@gmail.com>
2022-07-08 17:58:20 +0600

    What? api implementation : users/me/ How? 1. added UserProfileSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,16 @@
 from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
+from djoser.serializers import UserSerializer as Profile
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
 
 class UsersCreateSerializer(BaseUserRegistrationSerializer):
     class Meta(BaseUserRegistrationSerializer.Meta):
         model = get_user_model()
         fields = ['id', 'email', 'name', 'username', 'password', 'phone']
+
+
+class UserProfileSerializer(Profile):
+    class Meta(Profile.Meta):
+        model = get_user_model()
+        fields = ['id', 'email', 'name', 'username', 'phone']
