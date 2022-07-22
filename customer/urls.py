7cb07d4d181405b3b95dd2e4b71a304c3d8bec39
rootless <ripon1995dc@gmail.com>
2022-07-23 04:48:54 +0600

    What? api implementation : customer/details How? 1. one endpoint for create data 2. one endpoint for get data

--- customer/urls.py
+++ customer/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from customer import views
+
 # test
 urlpatterns = [
-    path('customers/', views.GetCustomerList.as_view()),
-    path('customers/<int:pk>/', views.GetCustomer.as_view()),
-    path('customers/signup/', views.CustomerSignUp.as_view()),
+    path('details/', views.CreateCustomerDetails.as_view()),
+    path('details/<int:pk>/', views.GetCustomerDetails.as_view())
 ]
