864c9ec0157ff689a0beef4a1eda7564267efbc4
rootless <ripon1995dc@gmail.com>
2022-07-23 04:47:39 +0600

    What? api implementation : customer/details How? 1. model changed

--- customer/models.py
+++ customer/models.py
@@ -2,8 +2,4 @@ from django.db import models
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(blank=False, unique=True)
-    phone = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    username = models.CharField(max_length=20)
+    profile_image = models.CharField(max_length=255)
