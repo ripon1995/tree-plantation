b84574471ea9b4d169ba5b33959ace65d9d97a1d
rootless <ripon1995dc@gmail.com>
2022-07-23 04:49:07 +0600

    What? api implementation : customer/details How? 1. updated customer endpoint

--- treeplant/urls.py
+++ treeplant/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('customer.urls')),
+    path('customer/', include('customer.urls')),
     path('auth/', include('users.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
