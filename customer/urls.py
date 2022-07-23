c1b934ea3a9de789c58491fdf1d4a8801963f273
rootless <ripon1995dc@gmail.com>
2022-07-23 06:58:56 +0600

    What? api implementation : customer/details How? 1. updated customer endpoint

--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from customer import views
 
-# test
 urlpatterns = [
     path('details/', views.CreateCustomerDetails.as_view()),
     path('details/<int:pk>/', views.GetCustomerDetails.as_view())
